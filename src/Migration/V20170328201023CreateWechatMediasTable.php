607dcbbf5c63547f1f165c955036e520bc41b51f
gbzhurui <790449591@qq.com>
2017-03-29 00:10:13 +0800

    fix

--- src/Migration/V20170328201023CreateWechatMediasTable.php
+++ src/Migration/V20170328201023CreateWechatMediasTable.php
@@ -11,17 +11,17 @@ class V20170328201023CreateWechatMediasTable extends BaseMigration
      */
     public function up()
     {
-        $this->schema->table('wechatMedias')
-            ->id()
-            ->int('appId')
-            ->string('wechatMediaId', 128)
-            ->tinyInt('type', 1)
-            ->tinyInt('temp', 1)->defaults(1)
-            ->string('path', 255)
-            ->string('wechatUrl', 255)
-            ->timestampsV1()
-            ->userstampsV1()
-            ->exec();
+//        $this->schema->table('wechatMedias')
+//            ->id()
+//            ->int('appId')
+//            ->string('wechatMediaId', 128)
+//            ->tinyInt('type', 1)
+//            ->tinyInt('temp', 1)->defaults(1)
+//            ->string('path', 255)
+//            ->string('wechatUrl', 255)
+//            ->timestampsV1()
+//            ->userstampsV1()
+//            ->exec();
     }
 
     /**
