d950124c063d5e91bfcdf007de809dc87de02a0a
twinh <twinhuang@qq.com>
2017-09-04 20:51:01 +0800

    解决直接访问地址错误 closes #3735

--- src/Controller/Admin/WechatMedias.php
+++ src/Controller/Admin/WechatMedias.php
@@ -3,14 +3,29 @@
 namespace Miaoxing\WechatMedia\Controller\Admin;
 
 use miaoxing\plugin\BaseController;
+use Wei\Request;
 
 class WechatMedias extends BaseController
 {
     /**
      * 将外部图片地址映射到微信图片地址
      */
-    public function createAction($req)
+    public function createAction(Request $req)
     {
+        $validator = wei()->validate([
+            // 如果传 $req 会导致调用 getUrl 而一直有值
+            'data' => $req->getParameterReference('post'),
+            'rules' => [
+                'url' => []
+            ],
+            'names' => [
+                'url' => 'Url'
+            ]
+        ]);
+        if (!$validator->isValid()) {
+            return $this->err($validator->getFirstMessage());
+        }
+
         $wechatUrl = wei()->wechatMedia->updateUrlToWechatUrl($req['url']);
 
         return $this->suc([
